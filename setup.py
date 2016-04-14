cefe21724ebf63e37d1ed01be8a58f92b1657e65
Dustin Rohde <xdrohdex@gmail.com>
2016-04-14 16:43:55 -0400

    Added README as long_description in setup

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name='rpn',
       version='1.0',
       description='Reverse Polish Notation calculator.',
+      long_description=readme(),
+      keywords='math calculator rpn arithmetic',
       url='https://github.com/dustinrohde/python-rpn',
       author='Dustin Rohde',
       author_email='xdrohdex@gmail.com',
