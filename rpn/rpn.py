0c4d85e3d37f2da91251dff841f5a70ba9a38f46
Dustin Rohde <xdrohdex@gmail.com>
2016-04-15 18:50:40 -0400

    Moved binary_ops, unary_ops, and number regexp to the module level.

--- rpn/rpn.py
+++ rpn/rpn.py
@@ -6,9 +6,26 @@ import math
 import operator as op
 import re
 
-# Increase outer limits for exponents as much as possible
+# Set outer limits for exponents to their limits
 decimal.DefaultContext.Emax = decimal.MAX_EMAX
 decimal.DefaultContext.Emin = decimal.MIN_EMIN
+
+# Binary operators
+binary_ops = {'+': Decimal.__add__, '-': Decimal.__sub__,
+              '*': Decimal.__mul__, '/': Decimal.__truediv__,
+              '%': Decimal.__mod__, '**': Decimal.__pow__,
+              '//': Decimal.__floordiv__}
+
+# Unary operators
+unary_ops = {'abs': Decimal.__abs__, 'sqrt': Decimal.sqrt,
+             'ceil': Decimal.__ceil__, 'floor': Decimal.__floor__}
+
+# Regular expression representing a basic number.
+number = re.compile(r'''-?  # optional minus sign
+                        \d* # zero or more digits
+                        \.? # optional decimal point
+                        \d+ # one or more digits''', re.VERBOSE)
+
 
 class RPNError(Exception):
     '''Exception for bad RPN input.'''
@@ -22,19 +39,6 @@ class RPNError(Exception):
 def solve_rpn(equation:str, context=decimal.DefaultContext) -> Decimal:
     '''Solve an arithmetic problem in Reverse Polish Notation.'''
     decimal.setcontext(context)
-
-    binary_ops = {'+': Decimal.__add__, '-': Decimal.__sub__,
-                  '*': Decimal.__mul__, '/': Decimal.__truediv__,
-                  '%': Decimal.__mod__, '**': Decimal.__pow__,
-                  '//': Decimal.__floordiv__}
-
-    unary_ops = {'abs': Decimal.__abs__, 'sqrt': Decimal.sqrt,
-                 'ceil': Decimal.__ceil__, 'floor': Decimal.__floor__}
-
-    number = re.compile(r'''-?  # optional minus sign
-                            \d* # zero or more digits
-                            \.? # optional decimal point
-                            \d+ # one or more digits''', re.VERBOSE)
 
     stack = []
     for unit in re.split(r'\s+', equation.strip()):
